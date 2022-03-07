6e094909567476a02793a4dcc0ffc16e96d3bbcd
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-03-07 15:36:23 +0200

    Form 1 completed

--- src/forms/price-calculations/PriceCalculationsService.ts
+++ src/forms/price-calculations/PriceCalculationsService.ts
@@ -7,15 +7,15 @@ type PriceCalculationServiceData = {
 export const PriceCalculationService = (): PriceCalculationServiceData => {
   const calculate = (price: number, weight: number, creditCard: boolean): number | undefined => {
     let deliveryFee = weight < 5 || price > 100 ? 0 : weight;
-    let result = price;
+    let result = price
     
     if (isNaN(price) || isNaN(weight)) return undefined;
     
-    if (price > 200) {
-      result = creditCard && weight < 5 ? result * 0.9 : result * 0.85
+    if (price >= 200) {
+      result = creditCard && weight < 5 ? result * 0.85 : result * 0.9
     }
     if (price < 200 && creditCard) {
-      result = result * 0.96;
+      result = result * 0.97;
     }
     
     return Math.round((result + deliveryFee) * 10) / 10;
